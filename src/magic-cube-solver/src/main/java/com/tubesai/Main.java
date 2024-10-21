76847fbc46f1183dcec382f785893ab65b6d5530
TazakiN <tazkia.nizami@gmail.com>
2024-10-21 23:55:24 +0700

    feat: setup maven project

--- src/magic-cube-solver/src/main/java/com/tubesai/Main.java
+++ src/magic-cube-solver/src/main/java/com/tubesai/Main.java
@@ -2,6 +2,6 @@ package com.tubesai;
 
 public class Main {
     public static void main(String[] args) {
-        System.out.println("Hello world!");
+        System.out.println("Magic Cube Solver is running...");
     }
 }
