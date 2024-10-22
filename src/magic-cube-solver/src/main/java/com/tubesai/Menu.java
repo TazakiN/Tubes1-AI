6d6a3ab59c504ed921e56267f4db405cca12a29c
Yusufarsan <sandiyusufardian@gmail.com>
2024-10-22 10:49:25 +0700

    feat: added Menu.java

---
+++ src/magic-cube-solver/src/main/java/com/tubesai/Menu.java
@@ -0,0 +1,18 @@
+package com.tubesai;
+
+public class Menu {
+    public int chooseAlgoMenu() {
+        // return 1 for Hill Climbing Side Move
+        // return 2 for Simulated Annealing
+        // return 3 for Genetic Algorithm
+
+        return 0;
+    }
+
+    public boolean isVisualize() {
+        // return true if user wants to visualize the cube
+        // return false if user doesn't want to visualize the cube
+
+        return false;
+    }
+}
