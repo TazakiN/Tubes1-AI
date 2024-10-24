aa84306bf2575cbdb7ce99bf4b31ca95c6fde4de
Yusufarsan <sandiyusufardian@gmail.com>
2024-10-24 15:04:41 +0700

    chore: add testing code for simulated annealing

--- src/magic-cube-solver/src/main/java/com/tubesai/Main.java
+++ src/magic-cube-solver/src/main/java/com/tubesai/Main.java
@@ -4,6 +4,18 @@ import java.util.Random;
 
 public class Main {
     public static void main(String[] args) {
-        System.out.println("Magic Cube Solver is running...");
+        // This just a test to see if the SimulatedAnnealing works
+        MagicCube cube = new MagicCube(5);
+
+        SimulatedAnnealing solver = new SimulatedAnnealing(100, 1);
+        MagicCube solvedCube = solver.getSolvedCube(cube);
+
+        System.out.println("Starter Cube:");
+        System.out.println("Fitness: " + cube.getFitness());
+        cube.printCube();
+        System.out.println();
+        System.out.println("Solved Cube:");
+        System.out.println("Fitness: " + solvedCube.getFitness());
+        solvedCube.printCube();
     }
 }
