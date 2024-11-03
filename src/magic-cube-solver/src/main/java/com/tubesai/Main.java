556eb63128d441f15e3e6476b3d437faff225598
TazakiN <tazkia.nizami@gmail.com>
2024-11-03 12:34:12 +0700

    fix: menu closed issue

--- src/magic-cube-solver/src/main/java/com/tubesai/Main.java
+++ src/magic-cube-solver/src/main/java/com/tubesai/Main.java
@@ -17,11 +17,11 @@ public class Main {
         int algoChoice = menu.chooseAlgoMenu();
         // TODO: handle algorithm choice
         if (algoChoice == 1) {
-            solver = new HillClimbingSideMove(10);      // Change max_side_moves as needed
+            solver = new HillClimbingSideMove(10); // Change max_side_moves as needed
         } else if (algoChoice == 2) {
             solver = new SimulatedAnnealing(10, 0.000001);
         } else if (algoChoice == 3) {
-            solver = new GeneticAlgorithm(1, 1,1);
+            solver = new GeneticAlgorithm(1, 1, 1);
         } else {
             System.out.println("Error in algorithm choice.");
             return;
@@ -36,25 +36,29 @@ public class Main {
         // Visualize the solved cube
         boolean visualize = menu.isVisualize();
         if (visualize) {
-            CubeVisualizer.visualize(solvedCube);
+            CubeVisualizer.visualize(solvedCube, GraphData.createRealisticDummyData(true));
         }
+
+        menu.closeScanner();
     }
-    
+
     // public static void main(String[] args) {
-    //     // This just a test to see if the SimulatedAnnealing works
-    //     MagicCube cube = new MagicCube(5);
+    // // This just a test to see if the SimulatedAnnealing works
+    // MagicCube cube = new MagicCube(5);
 
-    //     SimulatedAnnealing solver = new SimulatedAnnealing(10, 0.000001);
-        
-    //     System.out.println("Starter Cube:");
-    //     System.out.println("Fitness: " + cube.getFitness());
-    //     // cube.printCube();
-    //     System.out.println();
-    //     MagicCube solvedCube = solver.getSolvedCube(cube);
-    //     System.out.println("Solved Fitness (re-evaluate): " + solvedCube.evaluateObjFunc2());
-    //     System.out.println("Solved Fitness (fitness attribute): " + solvedCube.getFitness());
-    //     System.out.println();
+    // SimulatedAnnealing solver = new SimulatedAnnealing(10, 0.000001);
 
-    //     CubeVisualizer.visualize(solvedCube);
+    // System.out.println("Starter Cube:");
+    // System.out.println("Fitness: " + cube.getFitness());
+    // // cube.printCube();
+    // System.out.println();
+    // MagicCube solvedCube = solver.getSolvedCube(cube);
+    // System.out.println("Solved Fitness (re-evaluate): " +
+    // solvedCube.evaluateObjFunc2());
+    // System.out.println("Solved Fitness (fitness attribute): " +
+    // solvedCube.getFitness());
+    // System.out.println();
+
+    // CubeVisualizer.visualize(solvedCube);
     // }
 }
